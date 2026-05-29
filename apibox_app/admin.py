f497aa0c9a3f95262e038e566ce905b5dbe7c732
Enzo Raphael Fernandes Marinho <enzo57819216@edu.df.senac.br>
2026-05-28 21:38:57 -0300

    Atualiza o README com a documentação da API

--- apibox_app/admin.py
+++ apibox_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+#  registrar o modelo BOX para que ele apareça na interface de administração do Django
+from .models import box
+
+class BoxAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'numero')
+    search_fields=('numero',)
+admin.site.register(box, BoxAdmin)
